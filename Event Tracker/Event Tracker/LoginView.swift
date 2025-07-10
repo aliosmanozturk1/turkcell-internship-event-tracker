e7dfcf03778fe3837d301596d0b0c186eb9b8142
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-07-10 09:19:23 +0300

    feat: implement user registration view and navigation
    - Add complete RegisterView with form fields and styling
    - Update LoginView to use NavigationStack and NavigationLink to RegisterView
    - Add proper navigation titles and toolbar configuration

--- Event Tracker/Event Tracker/LoginView.swift
+++ Event Tracker/Event Tracker/LoginView.swift
@@ -12,95 +12,100 @@ struct LoginView: View {
     @State private var password: String = ""
     
     var body: some View {
-        ZStack {
-            Color(hex: "F2F1EC")
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack(spacing: 10) {
-                Text("Welcome")
-                    .font(.largeTitle)
+        NavigationStack {
+            ZStack {
+                Color(hex: "F2F1EC")
+                    .edgesIgnoringSafeArea(.all)
                 
-                Spacer()
-                
-                TextField("E-Mail", text: $email)
-                    .padding()
-                    .background(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                
-                SecureField("Password", text: $password)
-                    .padding()
-                    .background(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                
-                Button {
-                    // TODO: Login Button Action
-                } label: {
-                    Text("Login")
+                VStack(spacing: 10) {
+                    Text("Welcome")
+                        .font(.largeTitle)
+                    
+                    Spacer()
+                    
+                    TextField("E-Mail", text: $email)
                         .padding()
-                        .bold()
-                        .frame(maxWidth: .infinity)
-                        .background(Color(hex: "283C5F"))
-                        .foregroundStyle(Color.white)
+                        .background(Color.white)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
-                }
-                
-                HStack {
-                    Text("Don't have an account yet?")
-                    Button("Sign Up") {
-                        // TODO: Register Button Action
-                    }
-                    .foregroundStyle(Color(hex: "1D40B0"))
-                }
-                
-                Spacer()
-                
-                Button {
-                    // TODO: Continue With Apple
-                } label: {
-                    HStack(alignment: .center) {
-                        Image(systemName: "apple.logo")
-                        Text("Continue with Apple")
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    Button {
+                        // TODO: Login Button Action
+                    } label: {
+                        Text("Login")
+                            .padding()
                             .bold()
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hex: "283C5F"))
+                            .foregroundStyle(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
-                    .padding(.vertical)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(hex: "000000"))
-                    .foregroundStyle(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                }
-                
-                Button {
-                    // TODO: Continue With Google
-                } label: {
-                    HStack(alignment: .center) {
-                        Image("google")
-                            .resizable()
-                            .frame(width: 18, height: 18)
-                        Text("Continue with Google")
-                            .bold()
+                    
+                    HStack {
+                        Text("Don't have an account yet?")
+                        NavigationLink("Sign Up") {
+                            RegisterView()
+                        }
+                        .foregroundStyle(Color(hex: "1D40B0"))
                     }
-                    .padding(.vertical)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(hex: "000000"))
-                    .foregroundStyle(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                }
-                
-                Button {
-                    // TODO: Continue as Guest
-                } label: {
-                    Text("Continue as Guest")
-                        .bold()
+                    
+                    Spacer()
+                    
+                    Button {
+                        // TODO: Continue With Apple
+                    } label: {
+                        HStack(alignment: .center) {
+                            Image(systemName: "apple.logo")
+                            Text("Continue with Apple")
+                                .bold()
+                        }
                         .padding(.vertical)
                         .frame(maxWidth: .infinity)
-                        .background(Color(hex: "FEF3C7"))
-                        .foregroundStyle(Color.primary)
+                        .background(Color(hex: "000000"))
+                        .foregroundStyle(Color.white)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    
+                    Button {
+                        // TODO: Continue With Google
+                    } label: {
+                        HStack(alignment: .center) {
+                            Image("google")
+                                .resizable()
+                                .frame(width: 18, height: 18)
+                            Text("Continue with Google")
+                                .bold()
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(hex: "000000"))
+                        .foregroundStyle(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    
+                    Button {
+                        // TODO: Continue as Guest
+                    } label: {
+                        Text("Continue as Guest")
+                            .bold()
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hex: "FEF3C7"))
+                            .foregroundStyle(Color.primary)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    
+                    Spacer()
                 }
-                
-                Spacer()
+                .padding()
             }
-            .padding()
+            .navigationTitle("Login")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
 }
---
+++ Event Tracker/Event Tracker/RegisterView.swift
@@ -0,0 +1,63 @@
+//
+//  RegisterView.swift
+//  Event Tracker
+//
+//  Created by Ali Osman Öztürk on 9.07.2025.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color(hex: "F2F1EC")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 10) {
+                Text("Register")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                TextField("E-Mail", text: $email)
+                    .padding()
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                SecureField("Re-Enter Password", text: $password)
+                    .padding()
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Button {
+                    // TODO: Login Button Action
+                } label: {
+                    Text("Register")
+                        .padding()
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .background(Color(hex: "283C5F"))
+                        .foregroundStyle(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle("Register")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
