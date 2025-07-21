7dccca0b3b68edf09d9bafd3783a65ca86e51653
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-07-21 10:33:16 +0300

    feat(auth): define UserModel struct
    Create UserModel.swift to represent user data, including uid, firstName, and lastName. This model will be used throughout the app for user profile operations.

---
+++ Event Tracker/Event Tracker/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Event Tracker
+//
+//  Created by Ali Osman Öztürk on 21.07.2025.
+//
+
+import Foundation
+
+struct UserModel {
+    let uid: String
+    let firstName: String?
+    let lastName: String?
+}
